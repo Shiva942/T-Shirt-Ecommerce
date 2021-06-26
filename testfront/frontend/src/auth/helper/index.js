587f2f782248657cb5477b16c9830bfbd6b5e3ed
Shiva942 <30700067+Shiva942@users.noreply.github.com>
2021-06-26 17:48:25 +0530

    Authentication and Fetch API
    Added fetch API functions for communication with DB

---
+++ testfront/frontend/src/auth/helper/index.js
@@ -0,0 +1,68 @@
+import {API} from "../../backend";
+
+
+export const signin = user =>{
+    return fetch(`${API}/signup`,{
+        method: "POST",
+        headers:{
+            Accept:"application/JSON",
+            "Content-type":"application/JSON"
+        },
+        body:JSON.stringify(user)
+    })
+    .then( response =>{
+
+        return response.json()
+    })
+    .catch(error => console.log(error));
+}
+
+export const signup = user =>{
+    return fetch(`${API}/signup`,{
+        method: "POST",
+        headers:{
+            Accept:"application/JSON",
+            "Content-type":"application/JSON"
+        },
+        body:JSON.stringify(user)
+    })
+    .then( response =>{
+
+        return response.json()
+    })
+    .catch(error => console.log(error));
+}
+
+export const authenticate =(data,next)=>{
+    if(typeof window !== 'undefined')
+        {
+            localStorage.setItem("jwt", JSON.stringify(data));
+            next();
+    }
+}
+export const signout =(next)=>{
+    if(typeof window !== 'undefined')
+        {
+            localStorage.removeItem("jwt")
+            next();
+            return fetch(`${API}/signout`,{
+                method: "GET",
+            })
+            .then( response =>{
+                console.log("SIGNOUT SUCCESS");
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+export const isAutheticated =()=>{
+    if(typeof window == 'undefined'){
+        return false;
+    }
+    if(localStorage.getItem("jwt")){
+        return JSON.parse(localStorage.getItem("jwt"))
+    }
+    else{
+        return false;
+    }
+}
