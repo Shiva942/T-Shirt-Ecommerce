4c9fc6622e4ca231237acdfba9d1c922eeffbc2d
Shiva942 <30700067+Shiva942@users.noreply.github.com>
2021-06-26 14:01:49 +0530

    Unnecessary Files/ Bootstrap
    Removed few files which were not required and added bootstrap CDN in index.html

--- testfront/frontend/src/routes.js
+++ testfront/frontend/src/routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./core/Home";
-import Signup from "./user/Signup";
-import Signin from "./user/Signin";
+import Home from "./core/Home.js";
+import Signup from './user/Signup.js';
+import Signin from './user/Signin.js';
 
 const Routes = () => {
   return (
